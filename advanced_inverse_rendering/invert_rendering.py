e5cbde4b1e3d531d97bdec697f8c376c94b49f52
udemegane <cappuccino1204.ck@gmail.com>
2021-01-06 01:15:13 +0900

    きれいにした

--- advanced_inverse_rendering/invert_rendering.py
+++ advanced_inverse_rendering/invert_rendering.py
@@ -1,43 +1,61 @@
 import argparse
 import os
-
+import yaml
 import mitsuba
+
 mitsuba.set_variant('gpu_autodiff_rgb')
 
-#import mitsuba c++ modules
+# import mitsuba c++ modules
 from mitsuba.core import xml, Thread
 
-#import miitsuba python modules
+# import mitsuba python modules
 from mitsuba.python.autodiff import render, write_bitmap
 
-def run(scene_path, out_dir, spp, pp_name):
-    Thread.thread().file_resolver().append(scene_path)
-    scene = xml.load_file(scene_path)
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
+from optimisation import heightfield
+from mylib.util import EasyDict
 
-    image_raw = render(scene, spp)
-    crop_size = scene.sensors()[0].film().crop_size()
-    write_bitmap(out_dir + scene_path.split('.')[0] + '.png', image_raw, crop_size)
-    print('Write ' + out_dir + scene_path.split('.')[0] + '.png')
+
+def run(args):
+    Thread.thread().file_resolver().append(args.scene_dir)
+    scene = xml.load_file(args.scene_dir + 'scene.xml')
+    if not os.path.exists(args.out):
+        os.makedirs(args.out)
+
+    # image_raw = render(scene, args.spp)
+    # crop_size = scene.sensors()[0].film().crop_size()
+    # write_bitmap(args.out + args.scene.split('.')[0] + '.png', image_raw, crop_size)
+    # print('Write ' + args.out + args.scene.split('.')[0] + '.png')
+    if args.script + '.py' in os.listdir('./optimisation'):
+        eval(args.script + '.optimisation')(args, scene)
+    else:
+        print('Error: Invalid scripts "' + args.script + '"')
+
 
 def main():
     parser = argparse.ArgumentParser(
         description='Render Scene by Nvidia GPU.',
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument('--scene', default='../data/scene/invert_heightfield.xml')
-    parser.add_argument('--ref_image', default='../data/refimages/mitsuba_logo.exr')
-    parser.add_argument('--out', default='../data/output/invert_heightfield/')
-    parser.add_argument('--spp', default='4')
-    parser.add_argument('--lr', default='0.01')
-    parser.add_argument('--iter', default='100')
-    parser.add_argument('--pre_render_callback', default='None')
-    parser.add_argument('--post_process', default='None')
-    parser.add_argument('--configfile', default='None')
+    parser.add_argument('--scene_dir', default=None)
+    parser.add_argument('--ref_image', default=None)
+    parser.add_argument('--out', default=None)
+    parser.add_argument('--spp', type=int, default=None)
+    parser.add_argument('--lr', type=float, default=None)
+    parser.add_argument('--iter', type=int, default=None)
+    parser.add_argument('--configfile', default='./config/heightfield.yml')
+    parser.add_argument('--script', default=None)
     args = parser.parse_args()
 
-    run(**vars(args))
+    with open(args.configfile, 'r') as file:
+        tmp = yaml.load(file)
+        config = EasyDict(tmp)
+    config.spp = int(config.spp)
+    config.iter = int(config.iter)
+    config.lr = float(config.lr)
+
+
+
+    run(config)
 
 if __name__ == '__main__':
     main()
---
+++ advanced_inverse_rendering/optimisation/heightfield.py
@@ -0,0 +1,92 @@
+import time
+
+import enoki as ek
+import mitsuba
+
+mitsuba.set_variant('gpu_autodiff_rgb')
+
+from mitsuba.core import Thread, xml, UInt32, Float, Vector2f, Vector3f, Transform4f, ScalarTransform4f
+from mitsuba.render import SurfaceInteraction3f
+from mitsuba.python.util import traverse
+from mitsuba.python.autodiff import render, write_bitmap, Adam
+
+from mylib.util import ravel, unravel
+
+
+def optimisation(args, scene):
+    params = traverse(scene)
+    positions_buf = params['grid_mesh.vertex_positions_buf']
+    positions_initial = ravel(positions_buf)
+    normals_initial = ravel(params['grid_mesh.vertex_normals_buf'])
+    vertex_count = ek.slices(positions_initial)
+
+    # Create a texture with the reference displacement map
+    disp_tex = xml.load_dict({
+        "type": "bitmap",
+        "filename": "mitsuba_coin.jpg",
+        "to_uv": ScalarTransform4f.scale([1, -1, 1])  # texture is upside-down
+    }).expand()[0]
+
+    # Create a fake surface interaction with an entry per vertex on the mesh
+    mesh_si = SurfaceInteraction3f.zero(vertex_count)
+    mesh_si.uv = ravel(params['grid_mesh.vertex_texcoords_buf'], dim=2)
+
+    # Evaluate the displacement map for the entire mesh
+    disp_tex_data_ref = disp_tex.eval_1(mesh_si)
+
+    # Apply displacement to mesh vertex positions and update scene (e.g. OptiX BVH)
+    def apply_displacement(amplitude=0.05):
+        new_positions = disp_tex.eval_1(mesh_si) * normals_initial * amplitude + positions_initial
+        unravel(new_positions, params['grid_mesh.vertex_positions_buf'])
+        params.set_dirty('grid_mesh.vertex_positions_buf')
+        params.update()
+
+    # Apply displacement before generating reference image
+    apply_displacement()
+    # Render a reference image (no derivatives used yet)
+    image_ref = render(scene, spp=3)
+    crop_size = scene.sensors()[0].film().crop_size()
+    write_bitmap(args.out + 'out_ref.exr', image_ref, crop_size)
+    print("Write " + args.out + "out_ref.exr")
+
+    # Reset texture data to a constant
+    disp_tex_params = traverse(disp_tex)
+    disp_tex_params.keep(['data'])
+
+    # Reset
+    disp_tex_params['data'] = ek.full(Float, 0.25, len(disp_tex_params['data']))
+    disp_tex_params.update()
+
+    # Construct an Adam optimizer that will adjust the texture parameters
+    opt = Adam(disp_tex_params, lr=0.002)
+
+    time_a = time.time()
+
+    for it in range(args.iter):
+        # Perform a differentiable rendering of the scene
+        image = render(scene,
+                       optimizer=opt,
+                       spp=2,
+                       unbiased=True,
+                       pre_render_callback=apply_displacement)
+
+        write_bitmap(args.out + 'out_%03i.png' % it, image, crop_size)
+
+        # Objective: MSE between 'image' and 'image_ref'
+        ob_val = ek.hsum(ek.sqr(image - image_ref)) / len(image)
+
+        # Back-propagate errors to input parameters
+        ek.backward(ob_val)
+
+        # Optimizer: take a gradient step -> update displacement map
+        opt.step()
+
+        # Compare iterate against ground-truth value
+        err_ref = ek.hsum(ek.sqr(disp_tex_data_ref - disp_tex.eval_1(mesh_si)))
+        print('Iteration %03i: error=%g' % (it, err_ref[0]), end='\r')
+
+    time_b = time.time()
+
+    print()
+    print('%f ms per iteration' % (((time_b - time_a) * 1000) / args.iter))
+
